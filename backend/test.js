fca525381dfcc32c67bd648457d7d171a8e7f60c
Benjamin Orndorff <borff@Benjamins-MacBook-Pro.local>
2023-04-14 22:36:34 -0400

    updated github

--- backend/test.js
+++
@@ -1,8 +0,0 @@
-const { photoTranslate } = require('./PhotoTranslate.js');
-
-async function testPhotoTranslate() {
-  const result = await photoTranslate('./backend/images/pho.jpeg', 'JA');
-  console.log(result);
-}
-
-testPhotoTranslate();
